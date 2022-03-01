e9fc09819bcdaca0c9c98a0586377bfbdf94c120
Extreme-Programmer <99515253+Extreme-Programmer@users.noreply.github.com>
2022-03-01 15:08:47 -0500

    Update remote-server.sh

--- remote-server.sh
+++ remote-server.sh
@@ -14,31 +14,23 @@ echo "COPY id_rsa.pub /home/remote_user/.ssh/authorized_keys"                   
 echo "RUN  chown remote_user:remote_user -R /home/remote_user/.ssh && chmod 600 /home/remote_user/.ssh/authorized_keys"     >> Dockerfile
 echo "CMD  [ \"/usr/sbin/sshd\" , \"-D\" ]" >> Dockerfile
 
-docker build -t remote_server .
-docker run -d --name remote_server -it -p 58897:22 remote-server
-ssh -i ./id_rsa remote_user@localhost -p 58897
-rm -rf ./Dockerfile
-
-
-echo ""                                                                                                                      > userGuide.txt
 (ls userGuide.txt && rm -rf userGuide.txt) || echo "no action needed"
 echo "########filename:userGuide.txt"                                                                                       >> userGuide.txt
 echo "# get started with new ui in opened docker container:"                                                                >> userGuide.txt
-echo "wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf"                           >> userGuide.txt
-echo "wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf"                           >> userGuide.txt
-echo "wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf"                           >> userGuide.txt
-echo "wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf"                     >> userGuide.txt
-echo "sh -c '$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)'"                                  >> userGuide.txt
-echo "git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k"                                     >> userGuide.txt
-echo "echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc"                                                     >> userGuide.txt
-echo "chsh -s $(which zsh)"                                                                                                 >> userGuide.txt
+echo "# wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf"                           >> userGuide.txt
+echo "# wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf"                           >> userGuide.txt
+echo "# wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf"                           >> userGuide.txt
+echo "# wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf"                     >> userGuide.txt
+echo "# sh -c '\$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)'"                                  >> userGuide.txt
+echo "# git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k"                                     >> userGuide.txt
+echo "# echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc"                                                     >> userGuide.txt
+echo "# chsh -s \$(which zsh)"                                                                                                 >> userGuide.txt
 echo "# to use sudo <command>:"                                                                                             >> userGuide.txt
-echo "docker exec -it remote_server /bin/bash"                                                                              >> userGuide.txt
-echo "echo 'remote_user ALL=(ALL)  ALL' >> /etc/sudoers"                                                                    >> userGuide.txt
+echo "# docker exec -it remote_server /bin/bash"                                                                              >> userGuide.txt
+echo "# echo 'remote_user ALL=(ALL)  ALL' >> /etc/sudoers"                                                                    >> userGuide.txt
 echo "# to use sudo <command>:"                                                                                             >> userGuide.txt
-echo "docker exec -it remote_server /bin/bash"                                                                              >> userGuide.txt
-echo "echo 'remote_user ALL=(ALL)  ALL' >> /etc/sudoers"                                                                    >> userGuide.txt
-
+echo "# docker exec -it remote_server /bin/bash"                                                                              >> userGuide.txt
+echo "# echo 'remote_user ALL=(ALL)  ALL' >> /etc/sudoers"                                                                    >> userGuide.txt
 
 echo "+----------------------------+"                                                                                       >> userGuide.txt
 echo "| USERNAME         PASSWORD  |"                                                                                       >> userGuide.txt
@@ -48,8 +40,10 @@ echo "|----------------------------|"                                           
 echo "| REMOTE_USER      PASS1234  |"                                                                                       >> userGuide.txt
 echo "+----------------------------+"                                                                                       >> userGuide.txt
 
-
-
+docker build -t remote_server .
+docker run -d --name remote_server -it -p 58897:22 remote-server
+ssh -i ./id_rsa remote_user@localhost -p 58897
+rm -rf ./Dockerfile
 
 # #!/bin/bash
 # if [ -e x.txt ]
@@ -58,5 +52,4 @@ echo "+----------------------------+"                                           
 # else
 #     echo "nok"
 # fi
-
 #(ls x.txt && echo yes) || echo no
