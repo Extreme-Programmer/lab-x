ea81547f8cac59e0251b249ff323d6d1cb19010a
Extreme-Programmer <99515253+Extreme-Programmer@users.noreply.github.com>
2022-03-01 15:30:10 -0500

    Update remote-server.sh

--- remote-server.sh
+++ remote-server.sh
@@ -8,6 +8,7 @@ echo "RUN  mkdir -p /var/run/sshd"                                              
 echo "RUN  apt-get update && apt-get install -y openssh-server"                                                             >> Dockerfile
 echo "RUN  apt-get update && apt-get -y upgrade && apt-get install -y openjdk-8-jdk openssh-server sudo"                    >> Dockerfile
 echo "RUN  useradd -rm -d /home/remote_user -s /bin/bash remote_user && echo remote_user:pass1234 | chpasswd"               >> Dockerfile
+echo "RUN echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers"                                                              >> Dockerfile
 echo "RUN  echo 'root:root-user' | chpasswd #RUN  echo 'root-user' | passwd --stdin root"                                   >> Dockerfile
 echo "RUN  mkdir /home/remote_user/.ssh/ && chmod 700 /home/remote_user/.ssh"                                               >> Dockerfile
 echo "COPY remote-server-key.pub /home/remote_user/.ssh/authorized_keys"                                                    >> Dockerfile
@@ -21,10 +22,10 @@ echo "wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20
 echo "wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf"                           >> userGuide.txt
 echo "wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf"                           >> userGuide.txt
 echo "wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf"                     >> userGuide.txt
-echo "sh -c '\$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)'"                                  >> userGuide.txt
+echo "sh -c '\$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)'"                                 >> userGuide.txt
 echo "git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k"                                     >> userGuide.txt
 echo "echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc"                                                     >> userGuide.txt
-echo "chsh -s \$(which zsh)"                                                                                                 >> userGuide.txt
+echo "chsh -s \$(which zsh)"                                                                                                >> userGuide.txt
 echo "# to use sudo <command>:"                                                                                             >> userGuide.txt
 echo "docker exec -it remote_server /bin/bash"                                                                              >> userGuide.txt
 echo "echo 'remote_user ALL=(ALL)  ALL' >> /etc/sudoers"                                                                    >> userGuide.txt
