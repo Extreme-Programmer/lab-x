56d2f0892eded37f630314a648f080413e409a7e
Extreme-Programmer <99515253+Extreme-Programmer@users.noreply.github.com>
2022-03-12 14:21:04 -0500

    Update remote-server.sh

--- remote-server.sh
+++ remote-server.sh
@@ -4,10 +4,20 @@
 (ls userGuide.txt && rm -rf userGuide.txt) || echo "no action needed"
 yes '' | ssh-keygen -N '' -f ./remote-server-key  > /dev/null
 
-echo "FROM ubuntu:latest"                                                                                                   		                        >> Dockerfile
-echo "RUN  mkdir -p /var/run/sshd"                                                                                          		                        >> Dockerfile
-#echo "RUN  apt-get update && apt-get install -y openssh-server"                                                             		                        >> Dockerfile
-echo "RUN  apt-get update && apt-get -y upgrade && apt-get install -y openjdk-8-jdk openssh-server sudo git zsh vim"         		                        >> Dockerfile
+echo "FROM ubuntu:latest"                                                                                                                               >> Dockerfile
+echo "RUN  mkdir -p /var/run/sshd"                                                                                                                      >> Dockerfile
+#echo "RUN  apt-get update && apt-get install -y openssh-server"                                                                                        >> Dockerfile
+
+echo "ENV TZ=Asia/Dubai"                                                               >> Dockerfile
+echo "RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone"  >> Dockerfile
+
+echo "RUN  apt-get update && apt-get -y upgrade"                                                       >> Dockerfile
+
+echo "RUN  apt-get install -y openjdk-8-jdk openssh-server sudo git vim"                                    >> Dockerfile
+
+echo "RUN DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt-get -y install tzdata"                                                                          >> Dockerfile
+echo "RUN DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt-get -y install zsh"                                                                          >> Dockerfile
+
 echo "RUN  useradd -rm -d /home/remote_user -s /bin/bash remote_user && echo remote_user:pass1234 | chpasswd && adduser remote_user sudo"               >> Dockerfile
 echo "RUN  echo 'root:root-user' | chpasswd #RUN  echo 'root-user' | passwd --stdin root"                                                               >> Dockerfile
 echo "RUN  mkdir /home/remote_user/.ssh/ && chmod 700 /home/remote_user/.ssh"                                                                           >> Dockerfile
@@ -20,13 +30,16 @@ echo "RUN  wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloL
 echo "RUN  wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf -P /home/remote_user/.local/share/fonts/"         >> Dockerfile
 echo "RUN  wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf -P /home/remote_user/.local/share/fonts/"         >> Dockerfile
 echo "RUN  wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf -P /home/remote_user/.local/share/fonts/"   >> Dockerfile
-# echo "RUN  sh -c '\$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)'"                                                        >> Dockerfile
+
+echo "RUN  mkdir -p /home/remote_user/powerlevel10k"                                                                                                    >> Dockerfile
+
+# echo "RUN  sh -c '\$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)'"                                                      >> Dockerfile
 echo "RUN  wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh || true"                                               >> Dockerfile
 echo "RUN  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /home/remote_user/powerlevel10k"                                            >> Dockerfile
 echo "RUN  echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> /home/remote_user/.zshrc"                                                           >> Dockerfile
 
-echo "RUN  cd ~/powerlevel10k"                                                                                                                          >> Dockerfile
-echo "RUN  exec zsh"                                                                                                                                    >> Dockerfile
+#echo "RUN  cd ~/powerlevel10k"                                                                                                                          >> Dockerfile
+#echo "RUN  exec zsh"                                                                                                                                    >> Dockerfile
 
 echo "RUN  chsh -s \$(which zsh)"
 
@@ -68,4 +81,3 @@ rm -rf ./Dockerfile
 #     echo "nok"
 # fi
 #(ls x.txt && echo yes) || echo no
-
